7737ebbe73e1311c220bf6337a767bf34cc48390
maplepoet <goatyang1234@hotmail.com>
2021-10-28 00:25:54 +0800

    backup

--- web.sh
+++ web.sh
@@ -3,11 +3,11 @@
 . venv/bin/activate
 # sudo apachectl stop
 # brew services start php
-pip install -r requirements.txt
+pip3.8 install -r requirements.txt
 # pip freeze > requirements.txt
 export FLASK_ENV=development
 export FLASK_APP=app.py
-flask run
-# nohup python -m flask run --host=0.0.0.0 & 
+# flask run --host=0.0.0.0
+nohup flask run --host=0.0.0.0 & 
 # ps  -ef | grep flask
 # kill -9 PID
